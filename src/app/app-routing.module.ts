35666132fb1728671ea39d94c85d7d422f80c2ff
marina00matta <marinamedhat_19@hotmail.com>
2020-04-04 21:27:45 +0200

    Project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
